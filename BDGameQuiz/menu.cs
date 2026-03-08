cb6c812ddf2df8b873870883fcea46880c13869e
Tiaramisu <Tiaramisu@gmail.com>
2026-03-08 17:15:59 -0600

    Agregar archivos de proyecto.

---
+++ BDGameQuiz/menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BDGameQuiz
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+
+    }
+}
