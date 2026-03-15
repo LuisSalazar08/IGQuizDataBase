5ef380c0003d4b9355d225f345b6132b7ada3a59
LuisSalazar08 <luisssalazar0.0@gmail.com>
2026-03-15 14:00:40 -0600

    Añadido Tabla para ver los Scores de partidas pasadas

--- BDGameQuiz/resultados.Designer.cs
+++ BDGameQuiz/resultados.Designer.cs
@@ -32,9 +32,13 @@
             this.lblScore = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.btnMenu = new System.Windows.Forms.Button();
+            this.ScoresView = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ScoreJugador = new System.Windows.Forms.DataGridViewTextBoxColumn();
             resPanel = new System.Windows.Forms.TableLayoutPanel();
             resPanel.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ScoresView)).BeginInit();
             this.SuspendLayout();
             // 
             // resPanel
@@ -66,17 +70,17 @@
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33223F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33223F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33555F));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.78841F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.21159F));
             this.tableLayoutPanel1.Controls.Add(this.btnMenu, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ScoresView, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 228);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 96.34703F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.652968F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(794, 219);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
@@ -84,13 +88,45 @@
             // 
             this.btnMenu.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnMenu.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMenu.Location = new System.Drawing.Point(267, 3);
+            this.btnMenu.Location = new System.Drawing.Point(573, 3);
             this.btnMenu.Name = "btnMenu";
-            this.btnMenu.Size = new System.Drawing.Size(258, 103);
+            this.btnMenu.Size = new System.Drawing.Size(218, 205);
             this.btnMenu.TabIndex = 0;
             this.btnMenu.Text = "Menu";
             this.btnMenu.UseVisualStyleBackColor = true;
             this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click_1);
+            // 
+            // ScoresView
+            // 
+            this.ScoresView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ScoresView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ScoresView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.ScoreJugador});
+            this.ScoresView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ScoresView.Location = new System.Drawing.Point(3, 3);
+            this.ScoresView.Name = "ScoresView";
+            this.ScoresView.RowHeadersWidth = 51;
+            this.ScoresView.RowTemplate.Height = 24;
+            this.ScoresView.Size = new System.Drawing.Size(564, 205);
+            this.ScoresView.TabIndex = 1;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Nombre.DividerWidth = 3;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // ScoreJugador
+            // 
+            this.ScoreJugador.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ScoreJugador.HeaderText = "Score";
+            this.ScoreJugador.MinimumWidth = 6;
+            this.ScoreJugador.Name = "ScoreJugador";
+            this.ScoreJugador.ReadOnly = true;
             // 
             // resultados
             // 
@@ -104,6 +140,7 @@
             resPanel.ResumeLayout(false);
             resPanel.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ScoresView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -113,5 +150,8 @@
         private System.Windows.Forms.Label lblScore;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.DataGridView ScoresView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ScoreJugador;
     }
 }
